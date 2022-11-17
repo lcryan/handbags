da05cf39d5f9db43c3ab937d71d74a060da9aa10
LuciaRyan <lcryan06@gmail.com>
2022-11-17 11:18:43 +0100

    buttons in nav bar - solved. set third button on toBeDisabled {true}

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import Button from "./Components/Button";
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  );
+    return (
+        <>
+            <h1>Handbags & Purses</h1>
+            <nav>
+                <Button
+                    buttonText="to the collection"
+                />
+                <Button
+                    buttonText="shop all bags"
+                />
+                <Button
+                    buttonText="preorder"
+                    toBeDisabled={true}
+                />
+            </nav>
+        </>
+    );
 }
 
 export default App;
