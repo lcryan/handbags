5b2a089ee6ad298c3af1a2776e534f14c2a6e7ae
LuciaRyan <lcryan06@gmail.com>
2022-11-17 12:48:00 +0100

    pictures inserted don't show up on the webpage = this is still in progress otherwise labels in span and p working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
 import Button from "./Components/Button";
-
+import BagOne from "./assets/bag_1.png";
+import BagTwo from "./assets/bag_2.png";
+import BagThree from "./assets/bag_3.png";
+import BagFour from "./assets/bag_4.png";
 function App() {
     return (
         <>
@@ -22,30 +25,29 @@ function App() {
             <main>
 
                 <article>
-                    <span>
-                        <p>Best seller</p>
-                    </span>
+                    <span>Best seller</span>
+                    <img src='./assets/bag_1.png' alt={BagOne}/>
+                    <p>The handy bag</p>
+
                 </article>
 
 
                 <article>
-                    <span>
-                        <p>Best seller</p>
-                    </span>
+                    <span> Best seller </span>
+                        <img src='./assets/bag_2.png' alt={BagTwo} />
+                    <p>The stylish bag</p>
                 </article>
 
-
                 <article>
-                <span>
-                    <p>New collection</p>
-                </span>
+                    <span> New collection</span>
+                    <img src='./assets/bag_3.png' alt={BagThree} />
+                    <p>The simple bag</p>
                 </article>
 
-
                 <article>
-                    <span>
-                        <p>New collection</p>
-                    </span>
+                    <span>New collection</span>
+                        <img src='./assets/bag_4.png' alt={BagFour} />
+                    <p>The trendy bag</p>
                 </article>
 
             </main>
